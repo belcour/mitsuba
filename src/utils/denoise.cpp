fe2e3bdb6ee65808ceab6997d64f24b2165aa68d
Laurent Belcour <laurent.belcour@gmail.com>
2019-07-01 13:15:50 +0200

    [Scons] Compile a simple cross-bilateral filter if the OIID denoiser is not available [Util] Add a simple cross-bilaterial filter to the denoiser util

--- src/utils/denoise.cpp
+++ src/utils/denoise.cpp
@@ -30,9 +30,103 @@
 # include <omp.h>
 #endif
 
+#ifdef USE_OIID
 #include <OpenImageDenoise/oidn.hpp>
+#endif
 
 MTS_NAMESPACE_BEGIN
+
+/* Denoising classes */
+
+/* OpenImageDenoise */
+struct OpenImageDenoise
+{
+};
+
+/* Cross-Bilateral filter */
+struct CrossBilateral
+{
+   CrossBilateral(ref<Bitmap>& image,
+                  ref<Bitmap>& normal,
+                  ref<Bitmap>& albedo,
+                  ref<Bitmap>& depth)
+      : m_image(image),
+        m_normal(normal),
+        m_albedo(albedo),
+        m_depth(depth)
+   {
+   }
+
+   void run(ref<Bitmap> output) const
+   {
+      const int width  = output->getWidth();
+      const int height = output->getHeight();
+      #pragma omp parallel for collapse(2)
+      for(int i=0; i<width; ++i)
+         for(int j=0; j<height; ++j)
+         {
+            denoise_pixel(output, i, j);
+         }
+   }
+
+   inline
+   void denoise_pixel(ref<Bitmap>& output, int i, int j) const
+   {
+      // Constants
+      const int width  = output->getWidth();
+      const int height = output->getHeight();
+
+      const Point2i  p_ij      = Point2i(i,j);
+
+      const Spectrum albedo_ij = m_albedo->getPixel( p_ij );
+      const Spectrum normal_ij = m_normal->getPixel( p_ij );
+      const Spectrum depth_ij  = m_depth->getPixel( p_ij );
+
+      // Varying
+      Spectrum cum_value(0.0f);
+      float cum_weight = 0.0f;
+
+      for(int di = -w; di<=w; ++di)
+         for(int dj=-w; dj<=w; ++dj)
+         {
+            const int u = (i + di + width)  % width;
+            const int v = (j + dj + height) % height;
+
+            const Point2i p_uv = Point2i(u,v);
+
+            const Spectrum albedo_uv = m_albedo->getPixel( p_uv );
+            const Spectrum normal_uv = m_normal->getPixel( p_uv );
+            const Spectrum depth_uv  = m_depth->getPixel( p_uv );
+
+            Spectrum value  = m_image->getPixel( p_uv );
+            Float    weight = 1.0f;
+
+            weight *= exp( - m_inv_sigma_pixels * Float( di*di + dj*dj ) );
+            weight *= exp( - m_inv_sigma_albedo * powf((albedo_ij - albedo_uv).average(), 2.0f) );
+            weight *= exp( - m_inv_sigma_normal * powf((normal_ij - normal_uv).average(), 2.0f) );
+            weight *= exp( - m_inv_sigma_depth * powf((depth_ij - depth_uv).average(), 2.0f) );
+
+            cum_value  += weight*value;
+            cum_weight += weight;
+         }
+
+      cum_value /= cum_weight;
+      output->setPixel( p_ij, cum_value );
+   }
+
+   ref<Bitmap> m_image;
+   ref<Bitmap> m_normal;
+   ref<Bitmap> m_albedo;
+   ref<Bitmap> m_depth;
+
+   int w = 3;
+
+   Float m_inv_sigma_pixels = 0.1;
+   Float m_inv_sigma_albedo = 10.0;
+   Float m_inv_sigma_depth  = 10.0;
+   Float m_inv_sigma_normal = 10.0;
+
+};
 
 
 ref<Bitmap> loadImage(const std::string& filename) {
@@ -71,6 +165,7 @@ public:
         std::string outputFilename;
         std::string albedoFilename;
         std::string normalFilename;
+        std::string depthFilename;
 
         /* Parse command-line arguments
          * First element in argv list is the name of the plugin `denoise`
@@ -93,7 +188,6 @@ public:
                         break;
 
               case 'o':
-                        std::cout << " >> " << optarg << std::endl;
                         outputFilename = std::string(optarg);
                         break;
 
@@ -104,16 +198,16 @@ public:
               case 'n':
                         normalFilename = std::string(optarg);
                         break;
+
+              case 'd':
+                        depthFilename = std::string(optarg);
+                        break;
             }
         }
 
         if(outputFilename.empty()) {
             SLog(EError, "No output file specified! Use `-o filename` in the command line");
         }
-
-        std::cout << " >> using normal map: " << normalFilename << std::endl;
-        std::cout << " >> using albedo map: " << albedoFilename << std::endl;
-        std::cout << " >> using output map: " << outputFilename << std::endl;
 
         /* I/O variables
          *
@@ -145,11 +239,6 @@ public:
             outputFilename.replace(extPosition+1, 3, "exr");
         }
 
-
-        // Create an Open Image Denoise device
-        oidn::DeviceRef device = oidn::newDevice();
-        device.commit();
-
         /* Open files */
         ref<Bitmap> input  = loadImage(argv[argc-1]);
         Assert(input != nullptr);
@@ -161,10 +250,18 @@ public:
         ref<Bitmap> normal = loadImage(normalFilename);
         if(albedo != nullptr) normal = normal->convert(outputPixelFormat, Bitmap::EFloat32);
 
+        ref<Bitmap> depth = loadImage(depthFilename);
+        if(albedo != nullptr) depth = depth->convert(outputPixelFormat, Bitmap::EFloat32);
+
         // Create the output file
         ref<Bitmap> output = input->clone();
         const int width  = input->getWidth();
         const int height = input->getHeight();
+
+#ifdef USE_OIID
+        // Create an Open Image Denoise device
+        oidn::DeviceRef device = oidn::newDevice();
+        device.commit();
 
         // Create a denoising filter
         oidn::FilterRef filter = device.newFilter("RT"); // generic ray tracing filter
@@ -183,6 +280,10 @@ public:
         if (device.getError(errorMessage) != oidn::Error::None) {
             SLog(EError, errorMessage);
         }
+#else
+        CrossBilateral filter(input, albedo, normal, depth);
+        filter.run(output);
+#endif
 
         /* Output image */
         output = output->convert(outputPixelFormat, outputCompFormat, -1.0);
